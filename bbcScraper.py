7d577581c5fbbc96dedab2ccea1659f3ef20e7c3
codelord42069 <james.w.hayes@gmail.com>
2021-10-20 16:05:14 +0100

    swag

--- bbcScraper.py
+++ bbcScraper.py
@@ -6,51 +6,37 @@ r = requests.get('https://www.bbc.co.uk/news')
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(r.text, 'html.parser')
 
-results = soup.find_all('h3' , attrs={'class':'gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text'})
 
 
 hrefs=[]
+records = []
 
-for a in soup.find_all('a' , href=True, attrs={'class':'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor'}):
-    hrefs.append(a['href'])
-
-
-
-
-#[i['href'] for i in soup.find_all('a', href=True)]
-
-#hrefs = soup.find_all('h3' , attrs={'class':'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor',})
-
-#finds everything in span tag where class=short-desc
-
-
-#print(hrefs) 
-
-    
-
-
-
-records=[]
-for result in results:
-    lie = result
-    records.append(lie)
-
-#print (records) 
+for link in soup.find_all('a' , href=True, attrs={'class':'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor'}):
+    hrefs.append(link['href'])
+    heading = link.find('h3' , attrs={'class':'gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text'})
+    if heading:
+        headingText = heading.text
+    else:
+        headingText = ''
+    records.append(headingText)
+  
 
 
 
 
 
-d = {'col1': [records], 'col2': [hrefs]}
+
+
+
+d = {'col1': records, 'col2': hrefs}
 
 df = pd.DataFrame(data=d)
 
 
-print(df) 
 
 #df['date'] = pd.to_datetime(df['date'])
 
-#df.to_csv('bbc_headlines.csv', index=False, encoding='utf-8')
+df.to_csv('bbc_headlines.csv', index=False, encoding='utf-8')
 
 
 
